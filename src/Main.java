e10e9636a07efbb52daf0c26fc5d47c6527047ef
nikolay <n.ilin@it-park.tech>
2024-09-17 10:49:07 +0300

    hw 4.1

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-        String personOS = "iOS";
-        boolean clientOS = personOS == "iOS";
-        if (clientOS) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 18; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i > -11; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 1904; i < 2097; i += 4) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 7; i < 99; i += 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print((int) Math.pow(2, i) + " ");
+        }
+        System.out.println();
+
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total += 29000;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
 
-        String os2 = "Android";
-        short clientDeviceYear = 2014;
-        boolean clientOS2 = os2 == "iOS";
-        if (clientOS2 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS2 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (!clientOS2 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        int total2 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total2 += 29000;
+            total2 *= 1.01;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total2 + " рублей");
         }
 
-        short year = 2021;
-        if (year < 1584) {
-            System.out.println("Год должен быть больше, чем 1584");
-        } else if (year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else if (year % 100 == 0) {
-            System.out.println(year + " год не является високосным");
-        } else if (year % 4 == 0) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
-
-        byte deliveryDistance = 95;
-        if (deliveryDistance > 100) {
-            System.out.println("Доставка не доступна");
-        } else if (deliveryDistance > 60) {
-            System.out.println("Потребуется дней: 3");
-        } else if (deliveryDistance > 20) {
-            System.out.println("Потребуется дней: 2");
-        } else {
-            System.out.println("Потребуется дней: 1");
-        }
-
-        byte monthNumber = 12;
-        switch (monthNumber) {
-            case 1, 2, 12:
-                System.out.println("Зима");
-                break;
-            case 3, 4, 5:
-                System.out.println("Весна");
-                break;
-            case 6, 7, 8:
-                System.out.println("Лето");
-                break;
-            case 9, 10, 11:
-                System.out.println("Осень");
-                break;
-            default:
-                System.out.println("Некоректный номер месяца");
+        for (int i = 1; i < 11; i++) {
+            System.out.println("2*" + i + "=" + (i * 2));
         }
     }
 }
