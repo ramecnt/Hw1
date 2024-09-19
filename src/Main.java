3d4a3c4fd4633649ad49448dedde83848d3ece8a
nikolay <n.ilin@it-park.tech>
2024-09-19 11:22:58 +0300

    hw 5.2

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int[] mas = new int[3];
-        mas[0] = 1;
-        mas[1] = 2;
-        mas[2] = 3;
-        double[] mas2 = {1.57, 7.654, 9.986};
-        int[] mas3 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        System.out.print(mas[0]);
-        for (int i = 1; i < mas.length; i++) {
-            System.out.print(", " + mas[i]);
-        }
-        System.out.println();
-        System.out.print(mas2[0]);
-        for (int i = 1; i < mas2.length; i++) {
-            System.out.print(", " + mas2[i]);
-        }
-        System.out.println();
-        System.out.print(mas3[0]);
-        for (int i = 1; i < mas3.length; i++) {
-            System.out.print(", " + mas3[i]);
-        }
-        System.out.println();
-
-        System.out.print(mas[mas.length - 1]);
-        for (int i = mas.length - 2; i > -1; i--) {
-            System.out.print(", " + mas[i]);
-        }
-        System.out.println();
-        System.out.print(mas2[mas2.length - 1]);
-        for (int i = mas2.length - 2; i > -1; i--){
-            System.out.print(", " + mas2[i]);
-        }
-        System.out.println();
-        System.out.print(mas3[mas3.length - 1]);
-        for (int i = mas3.length - 2; i > -1; i--) {
-            System.out.print(", " + mas3[i]);
-        }
-        System.out.println();
-
+        int[] mas = {100, 200, 300, 433, 23};
+        int sum = 0;
         for (int i = 0; i < mas.length; i++) {
-            if (mas[i] % 2 == 1) {
-                mas[i] += 1;
+            sum += mas[i];
+        }
+        System.out.printf("Сумма трат за месяц составила %s рублей\n", sum);
+
+        int max = 0, min = 1000000;
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] > max) {
+                max = mas[i];
             }
-            System.out.print(mas[i] + " ");
+            if (mas[i] < min) {
+                min = mas[i];
+            }
+        }
+        System.out.printf("Минимальная сумма трат за неделю составила %s рублей\nМаксимальная сумма трат за неделю составила %s рублей\n", min, max);
+
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", (float) sum / mas.length);
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
         }
     }
 }
