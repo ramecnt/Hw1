3bbeb2f0f55645e8046264d330b4af4bbf897ca1
nikolay <n.ilin@it-park.tech>
2024-09-24 11:16:18 +0300

    hw 7

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,40 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        String firstName = "Ivan";
-        String lastName = "Ivanov";
-        String middleName = "Ivanovich";
-        String fullName = lastName + " " + firstName + " " + middleName;
-        System.out.println("Ф. И. О. сотрудника — " + fullName);
+    public static void isLeapOrNot(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
 
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    public static void osUpdate(boolean isIos, int year) {
+        String os;
+        if (isIos) {
+            os = "iOS";
+        } else {
+            os = "Android";
+        }
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear > year) {
+            System.out.println("Установите облегченную версию приложения для " + os + " по ссылке");
+        } else {
+            System.out.println("Установите обычную версию приложения для " + os + " по ссылке");
+        }
+    }
 
-        fullName = "Иванов Семён Семёнович".replace("ё", "е");
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
+    public static int deliveryDays(int distance) {
+        int result = 0;
+        if (distance < 20) {
+            result += 1;
+        } else if (distance < 60) {
+            result += 2;
+        } else if (distance < 100) {
+            result += 3;
+        } else {
+            result = -1;
+        }
+        return result;
     }
 }
