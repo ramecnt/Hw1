f4a4425c5d3bfd867b654f6efd065ab8cf49f3f9
nikolay <n.ilin@it-park.tech>
2024-09-27 15:55:49 +0300

    hw 8

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String name;
+    private final Author author;
+    private int year;
+
+    public Book(String name, int year, Author author) {
+        this.name = name;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,13 @@
-import java.time.LocalDate;
-
 public class Main {
-    public static void isLeapOrNot(int year) {
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
-    }
-
-    public static void osUpdate(boolean isIos, int year) {
-        String os;
-        if (isIos) {
-            os = "iOS";
-        } else {
-            os = "Android";
-        }
-        int currentYear = LocalDate.now().getYear();
-        if (currentYear > year) {
-            System.out.println("Установите облегченную версию приложения для " + os + " по ссылке");
-        } else {
-            System.out.println("Установите обычную версию приложения для " + os + " по ссылке");
-        }
-    }
-
-    public static int deliveryDays(int distance) {
-        int result = 0;
-        if (distance < 20) {
-            result += 1;
-        } else if (distance < 60) {
-            result += 2;
-        } else if (distance < 100) {
-            result += 3;
-        } else {
-            result = -1;
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
+        Author author1 = new Author("Ivanich", "Vasya");
+        Author author2 = new Author("Vasilich", "Ivan");
 
+        Book book1 = new Book("Похождения Васи", 2019, author1);
+        Book book2 = new Book("Похождения Вани", 2019, author2);
+
+        System.out.println(book1.getYear());
+        book1.setYear(2020);
+        System.out.println(book1.getYear());
     }
 }
